fb246986a8fd67ff7e884631e3e0df42ffaf41a7
bayun2445 <52805985+bayun2445@users.noreply.github.com>
2023-11-17 13:07:57 +0700

    added Legend data and assets

---
+++ lib/model/legend_data.dart
@@ -0,0 +1,146 @@
+class Legend {
+  String? name;
+  String? birthName;
+  String? born;
+  String? died;
+  String? yearsActive;
+  String? instrument;
+  String? popularSong;
+  String? summary;
+  String? image;
+
+  Legend({
+    this.name,
+    this.birthName,
+    this.born,
+    this.died,
+    this.yearsActive,
+    this.instrument,
+    this.popularSong,
+    this.summary,
+    this.image,
+  });
+}
+
+List<Legend> legendList = [
+  Legend(
+    name: "Duke Ellington",
+    birthName: "Edward Kennedy Ellington",
+    born: "April 29, 1899",
+    died: "May 24, 1974",
+    yearsActive: "1914–1974",
+    instrument: "Piano",
+    popularSong: "It Don't Mean a Thing",
+    summary:
+    "Duke Ellington was an American jazz composer, pianist, and bandleader who played a pivotal role in shaping the genre during the 20th century...",
+    image: "asset/image/duke_ellington.jpg",
+  ),
+  Legend(
+    name: "Thelonious Monk",
+    birthName: "Thelonious Sphere Monk",
+    born: "October 10, 1917",
+    died: "February 17, 1982",
+    yearsActive: "1940–1973",
+    instrument: "Piano",
+    popularSong: "Round Midnight",
+    summary:
+    "Thelonious Monk was an American jazz pianist and composer who was one of the pioneers of modern jazz...",
+    image: "asset/image/thelonious_monk.jpg",
+  ),
+  Legend(
+    name: "Miles Davis",
+    birthName: "Miles Dewey Davis III",
+    born: "May 26, 1926",
+    died: "September 28, 1991",
+    yearsActive: "1944–1975",
+    instrument: "Trumpet, Flugelhorn",
+    popularSong: "In a Silent Way",
+    summary:
+    "Miles Davis was an American jazz trumpeter, bandleader, and composer who was one of the most influential figures in the history of jazz...",
+    image: "asset/image/miles_davis.jpg",
+  ),
+  Legend(
+    name: "Billy Strayhorn",
+    birthName: "William Thomas Strayhorn",
+    born: "November 29, 1915",
+    died: "May 31, 1967",
+    yearsActive: "1934–1964",
+    instrument: "Piano",
+    popularSong: "Take The A Train",
+    summary:
+    "Billy Strayhorn was an American jazz composer, pianist, and arranger who is best known for his close collaboration with Duke Ellington...",
+    image: "asset/image/billy_strayhorn.jpg",
+  ),
+  Legend(
+    name: "Thad Jones",
+    birthName: "Thaddeus Joseph Jones",
+    born: "March 28, 1923",
+    died: "August 20, 1986",
+    yearsActive: "1954-1986",
+    instrument: "Trumpet, Flugelhorn",
+    popularSong: "A Child is Born",
+    summary:
+    "Thad Jones was an American jazz trumpeter, composer, and bandleader who was a prominent figure in the jazz scene during the mid-20th century...",
+    image: "asset/image/thad_jones.jpg",
+  ),
+  Legend(
+    name: "Charles Mingus",
+    birthName: "Charles Mingus Jr.",
+    born: "April 22, 1922",
+    died: "January 5, 1979",
+    yearsActive: "1943–1979",
+    instrument: "Double Bass, Piano",
+    popularSong: "Good Bye Pork Pie Hat",
+    summary:
+    "Charles Mingus was an American jazz double bassist, composer, and bandleader who was one of the most influential figures in the history of jazz...",
+    image: "asset/image/charles_mingus.jpg",
+  ),
+  Legend(
+    name: "Bill Evans",
+    birthName: "William John Evans",
+    born: "August 16, 1929",
+    died: "September 15, 1980",
+    yearsActive: "1950–1980",
+    instrument: "Piano",
+    popularSong: "Waltz for Debby",
+    summary:
+    "Bill Evans was an influential American jazz pianist and composer who played a significant role in the development of modern jazz piano...",
+    image: "asset/image/bill_evans.jpg",
+  ),
+  Legend(
+    name: "Count Basie",
+    birthName: "William James Basie",
+    born: "August 21, 1904",
+    died: "April 26, 1984",
+    yearsActive: "1924–1984",
+    instrument: "Piano, Organ",
+    popularSong: "One O'Clock Jump",
+    summary:
+    "Count Basie was a highly influential American jazz pianist, composer, and bandleader who played a major role in the swing era of jazz...",
+    image: "asset/image/count_basie.jpg",
+  ),
+  Legend(
+    name: "Mary Lou Williams",
+    birthName: "Mary Elfrieda Scruggs",
+    born: "May 8, 1910",
+    died: "May 28, 1981",
+    yearsActive: "1920–1981",
+    instrument: "Piano",
+    popularSong: "Roll 'Em",
+    summary:
+    "Mary Lou Williams was an influential American jazz pianist, composer, and arranger who played a key role in the development of bebop...",
+    image: "asset/image/mary_lou.jpg",
+  ),
+  Legend(
+    name: "Chick Corea",
+    birthName: "Armando Anthony Corea",
+    born: "June 12, 1941",
+    died: "February 9, 2021",
+    yearsActive: "1962–2021",
+    instrument: "Piano, Keyboard",
+    popularSong: "Spain",
+    summary:
+    "Chick Corea was an American jazz pianist, composer, and bandleader who was one of the most influential figures in the development of jazz fusion...",
+    image: "asset/image/chick_corea.jpg",
+  ),
+];
